0a03c59dbe36b5c74e51904204e490031d5af047
Joseph Jeremy C. Garcia <122107666+ZhayNaz@users.noreply.github.com>
2025-12-08 11:06:15 +0800

    first commit

---
+++ firebaseComfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_dkavb5DBR-66owus6g7jlVDnAAYuITY",
+  authDomain: "my-website-38123.firebaseapp.com",
+  projectId: "my-website-38123",
+  storageBucket: "my-website-38123.firebasestorage.app",
+  messagingSenderId: "619046006878",
+  appId: "1:619046006878:web:4b3069ae3f3910ca3084cf",
+  measurementId: "G-62T30KZL0P"
+};
